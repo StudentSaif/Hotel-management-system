ef3d65bd5c8389cb3639aa6b3a01e1b2c15d8675
Saif Ibrahim <engineersaifibrahim@gmail.com>
2024-05-30 10:22:58 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Your message has been sent successfully!');
+});
+
+document.getElementById('bookingform').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Your room booked successfully');
+});
+
